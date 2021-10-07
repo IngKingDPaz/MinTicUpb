b0c8941be310bdcf73a86963fc151b646e735c54
Reyner Muñoz <ing.reyner.de.paz@gmail.com>
2021-10-07 19:38:52 +0000

    este si es el ultimo

---
+++ Web/p.js
@@ -0,0 +1,21 @@
+console.log("hola mundo")
+
+var a=10
+let b=5
+var suma =a+b
+
+console.log("la suma es :" + suma)
+
+var dia=2
+switch (dia) {
+    case 1:
+       console.log("Domingo")
+        break;
+    case 2:
+        
+    console.log("Lunes")
+        break;    
+    
+    default:
+        console.log("no contempado")
+}
